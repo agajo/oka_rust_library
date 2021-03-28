d30b603cbd675eeb43ef5083b1acdb963c1a9edf
agajo <madaopc@gmail.com>
2021-03-28 15:12:41 +0900

    made itertools.rs

---
+++ src/bin/itertools.rs
@@ -0,0 +1,23 @@
+use itertools::Itertools;
+
+// itertoolsの使い方をいろいろ確認します。
+// itertools-num は別物です。
+
+fn main() {
+    // 組み合わせ全列挙 (indexだけでなく、実際の要素を指定した個数組み合わせてくれる)
+    let result = (0..5i64).combinations(3);
+    result.for_each(|v| {
+        println!("{:?}", v);
+    });
+    let v = vec!["ore".to_string(), "aitsu".to_string(), "senko".to_string()];
+    let result = v.iter().combinations(2);
+    result.for_each(|v| {
+        println!("{:?}", v);
+    });
+
+    // 同じものを選んでもいい組み合わせ全列挙
+    let result = (0..3i64).combinations_with_replacement(3);
+    result.for_each(|v| {
+        println!("{:?}", v);
+    });
+}
