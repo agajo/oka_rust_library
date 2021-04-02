2157262120e0acd8c3060ed25505d8fe29991d40
agajo <madaopc@gmail.com>
2021-04-02 10:01:25 +0900

    modified modular_struct.rs

--- src/bin/modular_struct.rs
+++ src/bin/modular_struct.rs
@@ -3,8 +3,8 @@ use std::fmt::{Display, Formatter, Result};
 use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 fn main() {
-    let x = Mn::new(4);
-    let y = Mn::new(10);
+    let x = Mn(4);
+    let y = Mn(10);
     println!(
         "x:{}, y:{}, x+y:{}, x*y:{}, x-y:{},y-x:{}",
         x,
@@ -15,10 +15,11 @@ fn main() {
         y - x,
     );
     println!("x³:{}, x¹⁰⁰⁰⁰⁰⁰⁰⁰:{}", x.pow(3), x.pow(100000000));
+    println!("3≡10:{}", Mn(3) == Mn(10));
     // TODO: max_nを変更
     let max_n = 10usize.pow(6);
-    let mut fac = vec![Mn::new(0); max_n];
-    let mut finv = vec![Mn::new(0); max_n];
+    let mut fac = vec![Mn(0); max_n];
+    let mut finv = vec![Mn(0); max_n];
     init_modular_tables(&mut fac, &mut finv);
     let a = modular_combination(6, 4, &fac, &finv);
     let b = modular_permutation(6, 4, &fac, &finv);
@@ -28,30 +29,24 @@ fn main() {
 }
 
 // --------------ここから、構造体Mn(Modular Number)の実装--------------
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Mn {
-    v: usize,
-    m: usize,
-}
+
+// TODO: CHANGE MODULAR HERE!!
+const MOD: usize = 7;
+// const MOD: usize = 998_244_353;
+// const MOD: usize = 1_000_000_007;
+
+// 中身がMOD未満であることは保証「「しません」」！！
+// 表示時は保証します。
+#[derive(Debug, Copy, Clone)]
+struct Mn(usize);
 
 impl One for Mn {
     fn one() -> Self {
-        Self {
-            v: 1,
-            // TODO: CHANGE MODULAR HERE!!
-            // m: 7,
-            m: 998_244_353,
-            // m:1_000_000_007,
-        }
+        Self(1)
     }
 }
 
 impl Mn {
-    fn new(value: usize) -> Mn {
-        let m = Mn::one().m;
-        let value = value.rem_euclid(m);
-        Mn { v: value, m: m }
-    }
     // これを10^7回以上呼ぶ場合はメモ化しましょう！！
     fn pow(&self, exp: usize) -> Mn {
         pow(*self, exp)
@@ -60,83 +55,67 @@ impl Mn {
 
 impl Display for Mn {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "{}", self.v)
+        write!(f, "{}", self.0.rem_euclid(MOD))
+    }
+}
+
+impl PartialEq for Mn {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.rem_euclid(MOD) == other.0.rem_euclid(MOD)
     }
 }
 
 impl Add for Mn {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Self {
-            v: (self.v + other.v).rem_euclid(self.m),
-            m: self.m,
-        }
+        Self((self.0 + other.0).rem_euclid(MOD))
     }
 }
 
 impl AddAssign for Mn {
     fn add_assign(&mut self, other: Self) {
-        *self = Self {
-            v: (self.v + other.v).rem_euclid(self.m),
-            m: self.m,
-        };
+        *self = Self((self.0 + other.0).rem_euclid(MOD));
     }
 }
 
 impl Mul for Mn {
     type Output = Self;
     fn mul(self, other: Self) -> Self {
-        Self {
-            v: (self.v * other.v).rem_euclid(self.m),
-            m: self.m,
-        }
+        Self((self.0 * other.0).rem_euclid(MOD))
     }
 }
 
 impl MulAssign for Mn {
     fn mul_assign(&mut self, other: Self) {
-        *self = Self {
-            v: (self.v * other.v).rem_euclid(self.m),
-            m: self.m,
-        };
+        *self = Self((self.0 * other.0).rem_euclid(MOD));
     }
 }
 
 impl Neg for Mn {
     type Output = Self;
     fn neg(self) -> Self {
-        Mn {
-            v: if self.v == 0 { 0 } else { self.m - self.v },
-            m: self.m,
-        }
+        Mn(MOD - self.0.rem_euclid(MOD))
     }
 }
 
 impl Sub for Mn {
     type Output = Self;
     fn sub(self, other: Self) -> Self {
-        Self {
-            v: if self.v >= other.v {
-                self.v - other.v
-            } else {
-                self.m - (other.v - self.v)
-            },
-
-            m: self.m,
-        }
+        Self(if self.0 >= other.0 {
+            self.0 - other.0
+        } else {
+            MOD - (other.0 - self.0).rem_euclid(MOD)
+        })
     }
 }
 
 impl SubAssign for Mn {
     fn sub_assign(&mut self, other: Self) {
-        *self = Self {
-            v: if self.v >= other.v {
-                self.v - other.v
-            } else {
-                self.m - (other.v - self.v)
-            },
-            m: self.m,
-        };
+        *self = Self(if self.0 >= other.0 {
+            self.0 - other.0
+        } else {
+            MOD - (other.0 - self.0).rem_euclid(MOD)
+        });
     }
 }
 
@@ -149,18 +128,17 @@ impl SubAssign for Mn {
 // 計算量 O(n)
 fn init_modular_tables(fac: &mut Vec<Mn>, finv: &mut Vec<Mn>) {
     let max_n = fac.len();
-    let mut inv = vec![Mn::new(0); max_n];
-    fac[0] = Mn::new(1);
-    fac[1] = Mn::new(1);
-    finv[0] = Mn::new(1);
-    finv[1] = Mn::new(1);
-    inv[1] = Mn::new(1);
+    let mut inv = vec![Mn(0); max_n];
+    fac[0] = Mn(1);
+    fac[1] = Mn(1);
+    finv[0] = Mn(1);
+    finv[1] = Mn(1);
+    inv[1] = Mn(1);
     for i in 2..max_n {
-        let mi = Mn::new(i);
-        let m = Mn::one().m;
+        let mi = Mn(i);
         fac[i] = fac[i - 1] * mi;
         // 何でこれで順にinv[i]が出るんや2020-06-30
-        inv[i] = Mn::new(m) - inv[m % i] * Mn::new(m / i);
+        inv[i] = Mn(MOD) - inv[MOD % i] * Mn(MOD / i);
         finv[i] = finv[i - 1] * inv[i];
     }
 }
@@ -168,12 +146,12 @@ fn init_modular_tables(fac: &mut Vec<Mn>, finv: &mut Vec<Mn>) {
 // 二項係数計算 nCk
 // 先にinitModularTables()を呼んでおくこと！
 fn modular_combination(n: usize, k: usize, fac: &Vec<Mn>, finv: &Vec<Mn>) -> Mn {
-    if fac[5] != Mn::new(120) {
+    if fac[5] != Mn(120) {
         println!("call initModularTables() first!!!!!!!!");
-        return Mn::new(0);
+        return Mn(0);
     }
     if n < k {
-        return Mn::new(0);
+        return Mn(0);
     }
     fac[n] * finv[k] * finv[n - k]
 }
@@ -181,12 +159,12 @@ fn modular_combination(n: usize, k: usize, fac: &Vec<Mn>, finv: &Vec<Mn>) -> Mn 
 // 順列計算 nPk
 // 先にinitModularTables()を呼んでおくこと！
 fn modular_permutation(n: usize, k: usize, fac: &Vec<Mn>, finv: &Vec<Mn>) -> Mn {
-    if fac[5] != Mn::new(120) {
+    if fac[5] != Mn(120) {
         println!("call initModularTables() first!!!!!!!!");
-        return Mn::new(0);
+        return Mn(0);
     }
     if n < k {
-        return Mn::new(0);
+        return Mn(0);
     }
     fac[n] * finv[n - k]
 }
