c470c0366ae8054d9a51597fd754bc484c15eed4
agajo <madaopc@gmail.com>
2021-05-16 15:30:21 +0900

    modified vializing files

--- visualize.js
+++ visualize.js
@@ -1,13 +1,56 @@
 window.onload = (event)=>{
-    e = document.getElementById("img_tag");
+    frame_input = document.getElementById("frame_input");
+    fps_input = document.getElementById("fps_input");
+    fps = 15;
+    fps_input.value = fps;
+
+    img_tag = document.getElementById("img_tag");
     i = 0;
     timer = setInterval(()=>{
-        e.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
+        img_tag.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
         i += 1;
-    },100);
-    e.onerror = ()=>{
+        frame_input.value=i;
+    },1000/fps);
+    img_tag.onerror = ()=>{
         clearInterval(timer);
         i-=1;
-        e.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
+        frame_input.value=i;
+        img_tag.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
     };
+
+    pause_button = document.getElementById("pause_button");
+    pause_button.onclick = ()=>{
+        clearInterval(timer);
+    }
+
+    play_button = document.getElementById("play_button");
+    play_button.onclick=()=>{
+        timer = setInterval(()=>{
+            img_tag.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
+            i += 1;
+            frame_input.value=i;
+        },1000/fps);
+    }
+
+    frame_input.oninput=()=>{
+        val = parseInt(frame_input.value);
+        if(!isNaN(val)){
+            i = val;
+            img_tag.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
+        }
+    }
+
+    fps_input.oninput=()=>{
+        val = parseInt(fps_input.value);
+        if(!isNaN(val)){
+            fps = val;
+        }
+    }
+
+    reset_button = document.getElementById("reset_button");
+    reset_button.onclick=()=>{
+        i=0;
+        frame_input.value=i;
+        img_tag.setAttribute("src","out_svg/"+("0000"+i).slice(-4)+".svg");
+    }
 }
